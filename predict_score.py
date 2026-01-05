75f73ff834525f81a703b2f6de14df20f1bfe705
CHIROOTH <chiroothgirigowda@gmail.com>
2026-01-05 12:17:11 -0700

    changing output to floating point scores

--- predict_score.py
+++ predict_score.py
@@ -92,11 +92,11 @@ def predict_item(model, tokenizer, item, device):
         probs = F.softmax(logits, dim=1)
         raw_prob = probs[0][1].item()
 
-    # score = round(raw_prob * 5)
-    if raw_prob < 0.0191888959936534: score = 2
-    elif raw_prob < 0.304312307: score = 3
-    elif raw_prob < 0.996154601: score = 4
-    else: score = 5
+    score = raw_prob * 5
+    # if raw_prob < 0.0191888959936534: score = 2
+    # elif raw_prob < 0.304312307: score = 3
+    # elif raw_prob < 0.996154601: score = 4
+    # else: score = 5
 
     # return int(score)
     return score
